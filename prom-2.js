22448957ceda6d04956b7ff4b5b4732643d97e5e
TaniaAilin <111372187+TaniaAilin@users.noreply.github.com>
2022-09-23 16:33:38 -0500

    Add files via upload
    prom-2

---
+++ prom-2.js
@@ -0,0 +1,11 @@
+function sumarUno(numero){
+    var promesa =new Promise(function(resolve, reject){
+       setTimeout(function(){ 
+        resolve(numero + 1);
+    }, 800);
+    })
+    return promesa;
+        }
+        sumarUno(5).then(function(numeroNuevo){
+            console.log(numeroNuevo);
+  });
