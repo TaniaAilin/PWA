596d1951a99b97c2c2515c7ae44d0c9cb057f72a
TaniaAilin <111372187+TaniaAilin@users.noreply.github.com>
2022-09-23 16:32:47 -0500

    Update prom-1.js
    add  3er codigo

--- prom-1.js
+++ prom-1.js
@@ -1,10 +1,9 @@
 /*function sumarUno(numero){
     return numero + 1;
 }
-
 console.log (sumarUno(15));*/
 
-function sumarUno(numero, callback){
+/*function sumarUno(numero, callback){
     setTimeout(function(){
         callback(numero + 1);
     }, 800);
@@ -13,6 +12,35 @@ sumarUno(5, function(nuevoValor){
     sumarUno(nuevoValor, function(nuevoValor2){
         sumarUno(nuevoValor2, function(nuevoValor3){
             console.log (nuevoValor3);
+        });
+    });
+});*/
+
+function sumarUno(numero, callback){
+    if (numero>=7){
+        callback('Número muy alto');
+        return;
+    }
+    setTimeout(function(){
+        callback(null, numero + 1);
+    }, 800);
+}
+sumarUno(5, function(error, nuevoValor){
+    if(error){
+        console.log(error);
+        return;
+    }
+    sumarUno(nuevoValor, function(error, nuevoValor2){
+        if(error){
+            console.log(error);
+            return;
+        }
+        sumarUno(nuevoValor2, function(error, nuevoValor3){
+            console.log (nuevoValor3);
+            if(error){
+                console.log(error);
+                return;
+            }
         });
     });
 });
