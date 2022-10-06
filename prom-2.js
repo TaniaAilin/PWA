ffe22860b0a67bd2797c7347b999154959d73eda
TaniaAilin <111372187+TaniaAilin@users.noreply.github.com>
2022-10-06 16:32:15 -0500

    Update prom-2.js
    add a prom 2

--- prom-2.js
+++ prom-2.js
@@ -1,11 +1,83 @@
-function sumarUno(numero){
-    var promesa =new Promise(function(resolve, reject){
+/*function sumarUno(numero){
+    var promesa = new Promise(function(resolve, reject){
        setTimeout(function(){ 
-        resolve(numero + 1);
+        resolve( numero + 1);
     }, 800);
-    })
+    });
     return promesa;
-        }
-        sumarUno(5).then(function(numeroNuevo){
-            console.log(numeroNuevo);
+}
+sumarUno( 5 ).then(nuevoNumero => {
+    sumarUno(nuevoNumero).then(nuevoNumero2 => {
+        console.log(nuevoNumero2);
   });
+});*/
+
+/*function sumarUno(numero){
+    var promesa = new Promise(function(resolve, reject){
+        if (numero>=7){
+            reject('El número es muy alto');
+        }
+       setTimeout(function(){ 
+        resolve( numero + 1);
+    }, 800);
+    });
+    return promesa;
+}
+sumarUno( 8 )
+    .then(nuevoValor => {
+        console.log(nuevoValor);
+    })
+        .catch(error=> {
+            console.log('Error en promesa');
+            console.log(error);
+    });*/
+/*
+    function sumarUno(numero){
+        var promesa = new Promise(function(resolve, reject){
+            if (numero>=7){
+                reject('El número es muy alto');
+            }
+           setTimeout(function(){ 
+            resolve( numero + 1);
+        }, 800);
+        });
+        return promesa;
+    }
+    sumarUno( 5 )
+        .then(sumarUno)
+        .then(sumarUno)
+        .then(sumarUno)
+    
+        .then(nuevoValor => {
+            console.log(nuevoValor);
+        })
+            .catch(error=> {
+                console.log('Error en promesa');
+                console.log(error);
+        });
+*/
+
+function sumarUno(numero){
+    var promesa = new Promise(function(resolve, reject){
+        console.log(numero)
+        if (numero>=7){
+            reject('El número es muy alto');
+        }
+       setTimeout(function(){ 
+        resolve( numero + 1);
+    }, 800);
+    });
+    return promesa;
+}
+sumarUno( 5 )
+    .then(sumarUno)
+    .then(sumarUno)
+    .then(sumarUno)
+
+    .then(nuevoValor => {
+        console.log(nuevoValor);
+    })
+        .catch(error=> {
+            console.log('Error en promesa');
+            console.log(error);
+    });
